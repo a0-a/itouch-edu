024a26e61e787dacbc0a519ec632017d1cd7fa3d
Adamu Abdulkarim <adamuabdulkarim3776@gmail.com>
2021-12-30 21:45:44 -0700

    make another change

--- itouch/urls.py
+++ itouch/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery, name='gallery'),
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
     path('view/<str:pk>/', views.viewPhoto, name='Photo'),
     path('add/', views.addPhoto, name='add'),
-    path('user_gallery', views.user_gallery, name='user_gallery'),
 ]
