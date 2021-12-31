363172e160a90048bb8d64ad0c1a1eb22853f015
Adamu Abdulkarim <adamuabdulkarim3776@gmail.com>
2021-12-31 10:42:22 -0700

    change debug

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y@p(9xmgqn(+0^(t3%v)nowqm6+4cqau1p+f$bj2m+gj^8*z$*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
